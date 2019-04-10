63dad0b0c05aba6e5b80538796991a307894e3e7
ehoseini <ehoseini@mit.edu>
2019-04-10 10:48:29 -0400

    just testing git

--- matlab/func_matlab_code.m
+++ matlab/func_matlab_code.m
@@ -2,4 +2,5 @@ function  [func_output]=func_matlab_code(job_id)
     func_output=num2str(job_id);
     fprintf(strjoin({'I am running Job # ', num2str(job_id)}));
     fprintf("end of job");
+    fprintf("testing git");
 end
